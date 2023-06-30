7950bc6ff083ed17184a05a3e3abd05b65266146
Lucas <79455308+Lucfost@users.noreply.github.com>
2023-06-30 15:59:08 +0400

    fix amk

--- src/function.h
+++ src/function.h
@@ -8,9 +8,9 @@ using namespace std;
 ///#define WIDTH  debug
 ///#define HEIGHT debug
 
-typedef	unsigned int			u32;
-typedef	__int32	          i32;
-typedef	unsigned char			Uint8;
+typedef	unsigned int			  u32;
+typedef	__int32	            i32;
+typedef	unsigned char			  Uint8;
 
 
 #endif // cxx 14  opt/disable	 
